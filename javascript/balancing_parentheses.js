c616102fd4c1a666fdcf07e7dfcfe5dc8118bc80
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-02 15:44:54 -0400

    brute force solution added

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,19 @@
 function balancingParentheses(string) {
-  // type your code here
+ 
+  let openP = string.split("").filter(x =>  x == ')')
+  let closedP = string.split("").filter(x => x == '(')
+  let needed = Math.abs(openP.length - closedP.length)
+  
+   if (string[string.length -1] == '(' && string[0] == ')') {
+    needed += 2
+  } else if (string[0] == ')' && needed == 0) {
+    needed += 1
+  } else if (string[string.length -1] == '(' && needed == 0) {
+    needed += 1
+  } 
+    
+  return needed
+  
 }
 
 if (require.main === module) {
@@ -22,3 +36,12 @@ module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// well. if there are only parentheses, don't worry about other characters.
+// 
+
+// the return must be the number of parentheses need to make it balanced. 
+// is this JUSt asking if there are thr same number of '(' and ')'??
+//try that solution 
+
+// what if it begins with a ) and ends with a ( then they'd be equal technically?? 
